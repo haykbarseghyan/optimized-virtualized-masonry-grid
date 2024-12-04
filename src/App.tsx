d865193ef5808559767fd0c59f2cb1a006a2f4c6
Hayk <haykbarsegyan@gmail.com>
2024-12-04 22:30:06 +0400

    Add eslint config, prettier & husky pre commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
       <h1>Optimized Virtualized Masonry Grid</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
